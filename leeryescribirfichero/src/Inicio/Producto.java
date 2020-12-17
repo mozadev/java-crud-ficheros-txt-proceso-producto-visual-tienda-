12ee6469e4399a221cd6ed93177fbe28c48f3d04
Cesar <30559667+CesarAroon@users.noreply.github.com>
2020-12-17 00:00:00 -0500

    Add files via upload

---
+++ leeryescribirfichero/src/Inicio/Producto.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Inicio;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author David
+ */
+public class Producto {
+    
+    private int codigo;
+    private String nombre;
+    private double precio;
+    private Object descripcion;
+
+    public Producto(){}
+    
+    public Producto(int codigo, String nombre, double precio, Object descripcion){
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.descripcion = descripcion;
+    }
+    /**
+     * @return the codigo
+     */
+    public int getCodigo() {
+        return codigo;
+    }
+
+    /**
+     * @param codigo the codigo to set
+     */
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the precio
+     */
+    public double getPrecio() {
+        return precio;
+    }
+
+    /**
+     * @param precio the precio to set
+     */
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    /**
+     * @return the descripcion
+     */
+    public Object getDescripcion() {
+        return descripcion;
+    }
+
+    /**
+     * @param descripcion the descripcion to set
+     */
+    public void setDescripcion(Object descripcion) {
+        this.descripcion = descripcion;
+    }
+    
+}
